9c6e7d18efd726d7dc968c2ef57b061c341e9f67
Sergei <sergo.mergo.1998@inbox.ru>
2023-07-29 06:48:08 +0300

    Решение5

---
+++ homework5.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class homework5 {
+    public static void main(String[] args) {
+        Map<String, List<String>> phonBook = new HashMap<>();
+        phonBook.put("Boris", List.of("+ 7 281", "+ 7 525"));
+        phonBook.put("Lev", List.of("+ 7 1852", "+ 7 2251"));
+        phonBook.put("Sergey", List.of("+ 78 282", "+ 78 152"));
+
+        menu(phonBook);
+
+    }
+
+    public static String scanner() {
+        Scanner scanner = new Scanner(System.in);
+        String scan = scanner.nextLine();
+        // scanner.close();
+        return scan;
+    }
+
+    public static void find(Map<String, List<String>> phonBook) {
+        System.out.println("Введите имя: ");
+        String name = scanner();
+        System.out.println(phonBook.get(name));
+    }
+
+    public static void allBook(Map<String, List<String>> phonBook) {
+        System.out.println(phonBook);
+
+    }
+
+    public static Map<String, List<String>> add(Map<String, List<String>> phonBook) {
+        System.out.println("Если хотите выйте хотите перестать вводить номера введите 'stop'");
+        System.out.println("Введите имя: ");
+        String name = scanner();
+        List<String> number = new ArrayList<>();
+        while (true) {
+            System.out.println("Введите номер: ");
+            String phon = scanner();
+            if (phon.equals("stop")) {
+                break;
+            } else {
+                number.add(phon);
+            }
+        }
+        phonBook.put(name, number);
+
+        return phonBook;
+    }
+
+    public static Map<String, List<String>> menu(Map<String, List<String>> phonBook) {
+        System.out.println(
+                "Введите команду из списка: find - найти контакт, add - добавить контакт, all - показать всю телефонную книгу, exit - выйти");
+        while (true) {
+            String comands = scanner();
+            if (comands.equals("exit")) {
+                break;
+            } else {
+                switch (comands) {
+                    case "find":
+                        find(phonBook);
+                        break;
+                    case "add":
+                        add(phonBook);
+                        break;
+                    case "all":
+                        allBook(phonBook);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        return phonBook;
+    }
+}
